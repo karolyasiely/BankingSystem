eb00f6277ded88e208fcd5bc25e51c8ab7aeabd3
karolyasiely <87988114+karolyasiely@users.noreply.github.com>
2023-10-22 15:14:10 -0500

    Update DebitCredit.java

--- DebitCredit.java
+++ DebitCredit.java
@@ -1,3 +1,59 @@
 public class DebitCredit {
+	Scanner scr = new Scanner(System.in); 
+    private double amountInAccount;
+    private double amountLoaned;
+    private String date;
+
+    void makeLoan(double loanAmount, String loanDate) {
+        amountLoaned += loanAmount;
+        date = loanDate;
+        System.out.println("Loan of $" + loanAmount + " made on " + loanDate + ".");
+    }
+
+    void payNextPayment(double paymentAmount) {
+        if (amountLoaned == 0) {
+            System.out.println("There is no loan amount available.");
+        } else {
+            amountLoaned -= paymentAmount;
+            System.out.println("$" + paymentAmount + " paid. Remaining loan amount: $" + amountLoaned + ".");
+        }
+    }
+
+    void setAmountInAccount(double amount) {
+        this.amountInAccount = amount;
+    }
+
+    double getAmountInAccount() {
+        return this.amountInAccount;
+    }
+}
+
+public class Credit {
+    private double amountInAccount;
+    private double amountLoaned;
+    private String date;
+
+    void makeLoan(double loanAmount, String loanDate) {
+        amountLoaned += loanAmount;
+        date = loanDate;
+        System.out.println("Loan of $" + loanAmount + " made on " + loanDate + ".");
+    }
+
+    void payNextPayment(double paymentAmount) {
+        if (amountLoaned == 0) {
+            System.out.println("There is no loan amount available.");
+        } else {
+            amountLoaned -= paymentAmount;
+            System.out.println("$" + paymentAmount + " paid. Remaining loan amount: $" + amountLoaned + ".");
+        }
+    }
+
+    void setAmountInAccount(double amount) {
+        this.amountInAccount = amount;
+    }
+
+    double getAmountInAccount() {
+        return this.amountInAccount;
+    }
     
 }
